443f74de87424baa15ffe559e810f292fc5723eb
squareleaf <tiffany.green@wellbe.me>
2017-08-11 13:23:11 -0500

    Chapter 2, Exercise 4 - mad libs

---
+++ 2.4-mad-lib.rb
@@ -0,0 +1,10 @@
+print "Enter a noun: "
+noun = gets.strip
+print "Enter a verb: "
+verb = gets.strip
+print "Enter an adjective: "
+adj = gets.strip
+print "Enter an adverb: "
+adv = gets.strip
+
+puts "Do you #{verb} your #{adj} #{noun} #{adv}? That's hilarious!"
